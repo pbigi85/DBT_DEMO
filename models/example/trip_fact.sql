61d490afb368b865d739683585918f31c410a13e
pbigi85.dev@gmail.com <pbigi85.dev@gmail.com>
2026-01-23 14:55:01 +0000

    fact trips erstellt

---
+++ models/example/trip_fact.sql
@@ -0,0 +1,17 @@
+with trips as (
+
+select
+	RIDE_ID,
+	RIDEABLE_TYPE,
+	date(to_timestamp(STARTED_AT))                                                  as Trip_date,  
+	START_STATIO_ID                                                                 as start_station_id,
+	END_STATION_ID                                                                  as end_station_id,
+	MEMBER_CSUAL                                                                    as MEMBER_CASUAL,                          
+    TIMESTAMPDIFF(Second, to_timestamp(STARTED_AT), to_timestamp(ENDED_AT))         as trip_duration
+from {{ source('demo', 'bike') }}
+where ride_id != 'ride_id'
+
+)
+
+select *
+from trips
